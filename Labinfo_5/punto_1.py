64eea7adf76e96144bce98a305e1c63c98e194c1
MatiwsxD <mateomejia401@gmail.com>
2019-02-28 20:13:05 -0500

    Intento punto 1

--- Labinfo_5/punto_1.py
+++ Labinfo_5/punto_1.py
@@ -1,5 +1,21 @@
+from sys import stdin 
 keys =[[1,2,3],
        [4,5,6],
        [7,8,9],
        ['*',0,'#'],]
-print(keys)
+
+row =[0,-1,0,1]
+col =[-1,0,1,0]
+pos_filas = [0,1,2,3,4,5,6,7,8,9]
+def cadenas(pos_fil,pos_colum,longitud):
+    if longitud == 1:
+        return keys[pos_fil][pos_colum]
+    else:
+        for i in range(len(row)):
+            return " "+ keys[cadenas(pos_fil+row[i],pos_colum+pos[colum],longitud-1)]
+def algo(longitud):
+    for i in range(pos_fila):
+        for j in range(pos_fila):
+            cadenas(i,j,longitud)
+longitud = 4
+    
